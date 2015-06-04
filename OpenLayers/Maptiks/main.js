29d480fb22e13ec8fafe1895c290d69be077a855
DannerTerra <breakearth@gmail.com>
2015-06-04 09:11:42 -0700

    Mastics with trackcode

--- OpenLayers/Maptiks/main.js
+++ OpenLayers/Maptiks/main.js
@@ -1,28 +1,19 @@
-maptiks.trackcode='e1ea1acb-e139-48eb-b105-4af2c373ba13';
-maptiks.debug=true;
+//maptiks.trackcode='e1ea1acb-e139-48eb-b105-4af2c373ba13';
+//maptiks.debug=true;
+
+
+//var marker = new ol.
+
 
 var map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'osm'})
-          }),
-          new ol.layer.Vector({
-            title: 'Earthquakes',
-            source: new ol.source.Vector() ,
-            style: new ol.style.Style({
-              image: new ol.style.Circle({
-                radius: 5,
-                fill: new ol.style.Fill({
-                  color: '#0000FF'
-                }),
-                stroke: new ol.style.Stroke({
-                  color: '#000000'
-                })
-              })
-            })
+            source: new ol.source.MapQuest({layer: 'osm'}),
+            maptiks_id:'LAYER'
           })
         ],
+        maptiks_id:'MAPA',
         view: new ol.View({
           center: ol.proj.transform([70.41, 2.82], 'EPSG:4326', 'EPSG:3857'),
           zoom: 3
