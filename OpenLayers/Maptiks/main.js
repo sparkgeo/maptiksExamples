47091ead7068bde6a83796b9ac2210882acca9fb
DannerTerra <breakearth@gmail.com>
2015-06-04 12:36:04 -0700

    OpenLayers with Marker

--- OpenLayers/Maptiks/main.js
+++ OpenLayers/Maptiks/main.js
@@ -1,21 +1,93 @@
 //maptiks.trackcode='e1ea1acb-e139-48eb-b105-4af2c373ba13';
 //maptiks.debug=true;
 
+var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point([0, 0]),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
 
-//var marker = new ol.
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon( ({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        //opacity: 0.75,
+        src: 'spkMarker.png'
+    }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+var markerSource = new ol.source.Vector({
+    features:[iconFeature]
+});
+
+var markerLayer = new ol.layer.Vector({
+  source: markerSource
+});
 
 
 var map = new ol.Map({
         target: 'map',
         layers: [
-          new ol.layer.Tile({
-            source: new ol.source.MapQuest({layer: 'osm'}),
-            maptiks_id:'LAYER'
-          })
+            new ol.layer.Tile({
+                source: new ol.source.MapQuest({layer: 'osm'}),
+                maptiks_id:'LAYER'
+            }),
+            markerLayer
         ],
         maptiks_id:'MAPA',
         view: new ol.View({
-          center: ol.proj.transform([70.41, 2.82], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 3
+            center: [0,0],
+            zoom: 3
         })
       });
+      
+/*
+
+      var iconFeature = new ol.Feature({
+  geometry: new ol.geom.Point([0, 0]),
+  name: 'Null Island',
+  population: 4000,
+  rainfall: 500
+});
+
+var iconStyle = new ol.style.Style({
+  image: new ol.style.Icon( ({
+    anchor: [0.5, 46],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    opacity: 0.75,
+    src: 'spkMarker.png'
+  }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+var vectorSource = new ol.source.Vector({
+  features: [iconFeature]
+});
+
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource
+});
+
+var rasterLayer = new ol.layer.Tile({
+  source: new ol.source.TileJSON({
+    url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp',
+    crossOrigin: ''
+  })
+});
+
+var map = new ol.Map({
+  layers: [rasterLayer, vectorLayer],
+  target: document.getElementById('map'),
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 3
+  })
+});
+*/
+
