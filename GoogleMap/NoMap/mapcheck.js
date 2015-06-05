2c075ecd3c44dfc621ee79e09d33b583978bfb72
Guilherme <gprsk95@gmail.com>
2015-06-05 14:39:51 -0700

    Code to detect GMaps

---
+++ GoogleMap/NoMap/mapcheck.js
@@ -0,0 +1,11 @@
+
+var x = document.querySelector('script[src="https://maps.googleapis.com/maps/api/js"]');
+
+if (x != undefined)
+{
+	console.log("there is a script");
+}
+else
+{
+	console.log("there is NOT a script");
+}
